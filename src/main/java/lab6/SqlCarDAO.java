9f6c5861e2d83579ab012649b067e3e152a5772a
Icell <olijnikv031@gmail.com>
2026-04-19 21:26:59 +0300

    lab6

--- src/main/java/lab6/SqlCarDAO.java
+++ src/main/java/lab6/SqlCarDAO.java
@@ -7,6 +7,27 @@ import java.util.List;
 
 public class SqlCarDAO implements CarDAO {
     @Override
+    public void save(Car car) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+
+            // Оце і все! Hibernate сам згенерує SQL-запит за тебе
+            session.save(car);
+
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public List<Car> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // Native SQL запит до ТАБЛИЦІ cars
+            String sql = "SELECT * FROM cars";
+            return session.createNativeQuery(sql, Car.class).getResultList();
+        }
+    }
+
+    @Override
     public List<Car> findByBrand(String brand) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             // Native SQL запит (до таблиці cars)
@@ -14,5 +35,7 @@ public class SqlCarDAO implements CarDAO {
             return session.createNativeQuery(sql, Car.class)
                     .setParameter("brandName", brand)
                     .getResultList();
+        }
+
     }
 }
