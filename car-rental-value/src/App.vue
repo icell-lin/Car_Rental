1a9b7a187e1dc93a63b72442608a8e21ef7091a4
Icell <olijnikv031@gmail.com>
2026-05-10 00:03:08 +0300

    lab8

--- car-rental-value/src/App.vue
+++ car-rental-value/src/App.vue
@@ -2,7 +2,7 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">🚗 CarRental</a>
+        <a class="navbar-brand" href="#">CarRental</a>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
